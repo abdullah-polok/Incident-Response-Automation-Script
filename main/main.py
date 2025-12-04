62460754154bc4905ef8da92abad5f567636f089
abdullah-polok <abdullahalrahman23@gmail.com>
2025-12-04 10:32:51 +0300

    add: Define systemInfo class inside systemInfo module and aalso define funtion for collecting info about OS

---
+++ main/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Incident Response and Threat Automation Tool running...")
+
+
+if __name__== "__main__":
+    main()    
---
+++ main/modules/systemInfo.py
@@ -0,0 +1,33 @@
+"""
+What it does in the real world:
+ This module collects basic information about a computer.
+ It tells you what operating system is running, how long the machine has been on, how much CPU and RAM it’s using, and who is logged in.
+
+Why it's useful in cybersecurity:
+ Attackers sometimes change system settings or run the machine under heavy load.
+ During an investigation, you need a quick summary to understand the machine’s status.
+
+Example real-world use:
+=>Check if the system is under stress (possible malware)
+
+=>Identify unauthorized users logged in
+
+=>Know how long the system has been running (helpful for timeline analysis)
+
+"""
+
+import platform  # try to retrieve system information
+import psutil    # for system and process utilities
+import datetime  # For handling date and time
+
+class SystemInfo:
+   #This module collects basic system information
+
+    def collectInfo(self):
+        #empty map for holding information
+        info={}
+        
+        #Assign Operating system info into the map
+        info["operating_system"]=platform.system()  # this function return OS name like windows, linux, etc.
+        info["operating_system_version"]=platform.version() # this function return system version
+        info["hostname"]=platform.node()  # this function return hostname of the machine
